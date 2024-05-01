fdb3b9e4bfa88cc8faa88e0f67b0c5a5103f0617
TausifM <heytausifsheikh@gmail.com>
2024-05-01 18:30:45 +0530

    exercise for date of birth

--- variables/variables.py
+++ variables/variables.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 rent = 8000
 groceries=41
 total = rent + groceries
@@ -7,3 +8,19 @@ print(total) # 8041
 # 4you = 4 --- Syntax error: invalid syntax
 # $error=404 --- Syntax error: invalid syntax
 # error%$error=501 --- Syntax error: invalid syntax
+foo=5
+bar='jalebi'
+foo=bar
+bar=foo
+id(foo)
+id(bar)
+
+
+# Assuming dob and todaydate are strings in the format dd/mm/yyyy
+dob = datetime.strptime('23/05/1994', '%d/%m/%Y')
+todaydate = datetime.strptime('01/05/2024', '%d/%m/%Y')
+
+# Calculating age
+age = todaydate.year - dob.year - ((todaydate.month, todaydate.day) < (dob.month, dob.day))
+
+print(age)
