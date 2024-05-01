7e127a9f01aea563fe43154c44bcb9633db6de5f
TausifM <heytausifsheikh@gmail.com>
2024-05-01 18:11:51 +0530

    error in variables

--- variables/variables.py
+++ variables/variables.py
@@ -1,3 +1,6 @@
 rent = 8000
-groceries=5
-total = 1202
+groceries=41
+total = rent + groceries
+print(total) # 8041
+# True= 7 ---- Cannot assigned to reserved key words
+# foo+bar=5 ---- cannot assigned to operator
