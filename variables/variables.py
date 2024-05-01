06ab6a15817f9a0b14ce408cf75e91cc17e83657
TausifM <heytausifsheikh@gmail.com>
2024-05-01 18:15:17 +0530

    systax error

--- variables/variables.py
+++ variables/variables.py
@@ -4,3 +4,6 @@ total = rent + groceries
 print(total) # 8041
 # True= 7 ---- Cannot assigned to reserved key words
 # foo+bar=5 ---- cannot assigned to operator
+# 4you = 4 --- Syntax error: invalid syntax
+# $error=404 --- Syntax error: invalid syntax
+# error%$error=501 --- Syntax error: invalid syntax
