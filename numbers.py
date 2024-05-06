2b95907fdb9ec3dab84dfed5cddf4e6563802d28
TausifM <heytausifsheikh@gmail.com>
2024-05-06 15:40:56 +0530

    add multiply

--- numbers.py
+++ numbers.py
@@ -9,3 +9,9 @@ print(area_of_triangle) # type: ignore
 # round method to make decimal points in round digits like round(number to be round, how many decimal points)
 rounded = round(area_of_triangle, 0) # 13.5 converted to 14
 print(rounded)
+
+# python do multiplication first
+10+2*3 # 16
+
+# if you want addition first then use ()
+(10+2) * 3 # 36
