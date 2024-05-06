08a6dcc9dd006891fcbfec85902040eb76688da6
TausifM <heytausifsheikh@gmail.com>
2024-05-06 15:38:51 +0530

    round method

---
+++ numbers.py
@@ -0,0 +1,11 @@
+# triangle area 1/2 * base * height
+
+base = 9
+height = 3
+
+area_of_triangle = (1/2)*base*height #13.5
+print(area_of_triangle) # type: ignore
+
+# round method to make decimal points in round digits like round(number to be round, how many decimal points)
+rounded = round(area_of_triangle, 0) # 13.5 converted to 14
+print(rounded)
