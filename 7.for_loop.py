5d00ff51545704d22d1da96abea1d4582ab09708
TausifM <heytausifsheikh@gmail.com>
2024-05-08 21:37:52 +0530

    exercise for loop

--- 7.for_loop.py
+++ 7.for_loop.py
@@ -32,3 +32,26 @@ n=0
 while n<=10:
     print(n)
     n=n+1
+    
+# range() function
+print(range(1,11))
+print(list(range(1,11)))
+for i in range(1,11):
+    print(i)
+    
+    
+# 4. Lets say you are running a 5 km race. Write a program that,
+#    1. Upon completing each 1 km asks you "are you tired?"
+#    2. If you reply "yes" then it should break and print "you didn't finish the race"
+#    3. If you reply "no" then it should continue and ask "are you tired" on every km
+#    4. If you finish all 5 km then it should print congratulations message
+
+for i in range(5):
+    print(f'{i+1} miles keep moving')
+    tired = input('Are you tired')
+    if tired == 'yes':
+        break
+    if i == 4:
+        print('Hurray you just finish the race')
+    else:
+        print(f'You run to end but you can try later {i+1} km acheived')
