baa7bdd912d21481677e873bbec74488c559990e
TausifM <heytausifsheikh@gmail.com>
2024-05-20 18:06:05 +0530

    dunder magic methods

--- advance-python/magic_methods_dunder.py
+++ advance-python/magic_methods_dunder.py
@@ -8,4 +8,22 @@ class Person:
 
 p = Person('Test', 25)
 
+class Vector:
+    def __init__(self, x, y) :
+        self.x = x
+        self.y = y
+
+    def __add__(self, other): 
+        return Vector(self.x + other.x, self.y + other.y)
+    def __repr__(self):
+        return f'X: {self.y}, Y: {self.y}'
+    def __call__(self):
+        print('Hello I was called')
+
+v1 = Vector(10,20)
+v2 = Vector(30,40)
+v3 = Vector(50,60)
+
+#v2()
+v3()
 ## advace python video by neuralnine
